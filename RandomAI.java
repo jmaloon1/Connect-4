ff55e4eb62fa554fef8d01fd15f07d2766c921e8
jmaloon1 <46735644+jmaloon1@users.noreply.github.com>
2020-01-08 10:15:06 -0800

    Add files via upload
    Initial commit. AI that plays random legal moves.

---
+++ RandomAI.java
@@ -0,0 +1,24 @@
+package hw4;
+import java.util.Random;
+
+public class RandomAI implements CFPlayer{
+	//fix this to make it only play legal moves
+	
+		public int nextMove(CFGame g) {
+		
+		int[][] gameState = g.getState();
+		Random rand = new Random();
+		int c = rand.nextInt(7);
+		
+		while(gameState[c][g.getNumRows()-1] != 0 && !g.isGameOver()) {		//checks to see whether the move will be playable
+			
+			c = rand.nextInt(7);		//re-randomizes variable until it makes a legal move
+		}	
+		return c+1; 
+	}
+	
+	public String getName() {
+		
+		return "Random Player";
+	}
+}
